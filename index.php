3b119ce2e1ae0586da031a81bb89b4ee3e6d3b14
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2021-06-03 18:15:46 -0300

    Modificações visuais
    Novo botão para salvar compromisso, data e hora em formato correto "D/M/Y", contador de dias para compromisso e botões de exclusão e edição (ainda não funcionais).

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
 include('inc/conecta.php');
-
+	$_GET['cd'] = 0;
+	$_GET['c'] = 0;
 	$cont = 0;
 	
+	date_default_timezone_set('America/Sao_Paulo');
+	$dthj = date('Y-m-d');
+
+	$atual = new DateTime();
+	$atual->format('Y-m-d');
+	
+
+
 	$sel = "SELECT * FROM compromisso as c INNER JOIN importancia as i on c.id_importancia = i.cd_importancia ORDER BY dt_compromisso ASC";
 
 	if($result = $mysqli->query($sel)){
@@ -14,6 +23,7 @@ include('inc/conecta.php');
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="css/inicial.css">
 	<title>Agenda</title>
 </head>
 <body>
@@ -25,24 +35,42 @@ include('inc/conecta.php');
 		      <th scope="col">#</th>
 		      <th scope="col">Nome</th>
 		      <th scope="col">Descrição</th>
-		      <th scope="col">Data e Hr</th>
+		      <th scope="col">Data e Hora</th>
 		      <th scope="col">Importância</th>
+		      <th scope="col">Opções</th>
 		    </tr>
 		  </thead>
 		  <tbody>
 		    <tr>
+
 		    	<?php
 
 		    	while($obj = $result->fetch_object()){
 
 		    		$cont ++;
+		    		$date = new DateTime($obj->dt_compromisso);
+		    		$dateInterval = $atual->diff($date);
 
 		    		echo "
 		    		<th scope='row'>".$cont."</th>
 		    		<td>".$obj->nm_compromisso."</td>
 		    		<td>".$obj->ds_compromisso."</td>
-		    		<td>".$obj->dt_compromisso."</td>
+		    		<td>".$date->format('d/m/Y H:i:s');
+
+		    		if($date>$atual){
+		    			echo " (faltam ".$dateInterval->days." dias)"; 
+		    		}
+		    		else if($atual > $date & $dateInterval->days != 0){
+		    			echo " (passaram ".$dateInterval->days." dias)"; 
+		    		}
+		    		if($dateInterval->days == 0){
+		    			echo " (HOJE)";	
+		    		}
+
+		    	echo "</td>
 		    		<td>".$obj->nm_importancia."</td>
+		    		<td><a href='delete.php?cd='".$obj->cd_compromisso."' class='btn btn-danger'>Excluir</a>
+		    		<a href='edit.php?c='".$obj->cd_compromisso."' class='btn btn-warning' id='editbtn'>Editar</a></td>
 		    		</tr>
 		    		";
 		    	}
@@ -57,28 +85,28 @@ include('inc/conecta.php');
 
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-12">
+			<div class="col-sm-12" id="inputs">
 				<form method="post">
 				<label>Insira o Nome do Compromisso:</label>
-				<input type="text" name="nome" class="form-control">
+				<input type="text" name="nome" class="form-control" id="inpt">
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-sm-12">
+			<div class="col-sm-12" id="inputs">
 				<label>Insira uma Descrição de Seu Compromisso:</label>
-				<input type="textarea" name="descricao" class="form-control">
+				<input type="textarea" name="descricao" class="form-control" id="inpt">
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-sm-12">
+			<div class="col-sm-12" id="inputs">
 				<label>Informe a Data e Horário de seu Compromisso:</label>
-				<input type="datetime-local" name="data" class="form-control">
+				<input type="datetime-local" name="data" class="form-control" id="inpt">
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-sm-12">
+			<div class="col-sm-12" id="inputs">
 				<label>Informe a Importância desse compromisso:</label>
-				<select class="form-control" name="imp">
+				<select class="form-control" name="imp" id="inpt">
 					<?php 
 						$imp = "SELECT * FROM importancia";
 
@@ -92,10 +120,10 @@ include('inc/conecta.php');
 			</div>
 		</div>
 		<div class="row" id="bt">
-			<div class="col-sm-12">
-				<input type="submit" value="Salvar" class="btn btn-success" name="">
+			<center><div class="col-sm-12">
+				<input type="submit" value="Salvar" class="btn btn-success" name="" id="btnslv">
 				</form>
-			</div>
+			</div></center>
 		</div>
 	</div>
 
