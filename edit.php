37bc435341d5ae59dd40e4e41860f3222a643b0e
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2021-06-09 11:08:44 -0300

    Funcao de editar compromisso funcional.
    Agora a funcao de editar compromisso esta disponivel, possibilitando modificar os dados presentes no banco.

--- edit.php
+++ edit.php
@@ -7,6 +7,7 @@ include('inc/conecta.php');
 	if($result = $mysqli->query($dados)){
 		while($obj = $result->fetch_object()){
 			//Define como variaveis os dados do compromisso
+			$cd = $obj->cd_compromisso;
 			$NomeComp = $obj->nm_compromisso;
 			$dsComp = $obj->ds_compromisso;
 			$dtComp = $obj->dt_compromisso;
@@ -19,6 +20,7 @@ include('inc/conecta.php');
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="css/edit.css">
 	<title>Editar compromisso</title>
 </head>
 <body>
@@ -29,19 +31,20 @@ include('inc/conecta.php');
 				<?php echo "<h3> Editar o Compromisso: ".$NomeComp." </h3>" ?>
 			</div>
 		</div>
-		<div class="row">
+		<div class="row" id="inpts">
 			<div class="col-sm-6">
+				<form method="post">
 				<label>Digite o Nome:</label>
 				<?php echo '<input type="text" class="form-control" name="nome" value="'.$NomeComp.'">' ?>
 			</div>
 			<div class="col-sm-6">
 				<label>Digite a Descrição:</label>
-				<?php echo '<input type="text" class="form-control" name="desc" value="'.$dsComp.'">' ?>
+				<?php echo '<input type="text" class="form-control" name="desc" value="'.$dsComp.'">'; ?>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-sm-6">
-				<label>Selecione a Data:</label>
+				<label>Selecione a Data:<?php echo "anteriormente (".$dtComp.")"; ?></label>
 				<?php
 					echo '<input type="datetime-local" class="form-control" name="data" value="'.$dtComp.'">';
 				?>
@@ -50,9 +53,9 @@ include('inc/conecta.php');
 				<label>Selecione a importância:</label>
 				<select class="form-control" name="imp">
 					<?php 
-						$imp = "SELECT * FROM importancia";
+						$impo = "SELECT * FROM importancia";
 
-						if($result = $mysqli->query($imp)){
+						if($result = $mysqli->query($impo)){
 							while($obj = $result->fetch_object()){
 								echo "<option value='$obj->cd_importancia' ";
 
@@ -61,7 +64,7 @@ include('inc/conecta.php');
 								}
 								echo ">$obj->nm_importancia</option>";
 								
-							echo "<option value='$obj->cd_categoria' ";if($categor == $obj->cd_categoria){ echo 'selected';} echo" >$obj->nm_categoria</option>";
+							//echo "<option value='$obj->cd_compromisso' ";if($imp == $obj->cd_importancia){ echo 'selected';} echo" >$obj->nm_compromisso</option>";
 	
 							}
 						}
@@ -69,7 +72,29 @@ include('inc/conecta.php');
 				</select>
 			</div>
 		</div>
+		<div class="row" id="salv">
+			<div class="col-sm-12">
+				<center><input type="submit" name="" value="Editar" class="btn btn-success"></center>
+				</form>
+			</div>
+		</div>
 	</div>
+
+<?php
+
+	if(isset($_POST['nome'])){
+
+		$edtar = "UPDATE compromisso SET nm_compromisso = '".$_POST['nome']."', ds_compromisso = '".$_POST['desc']."', dt_compromisso = '".$_POST['data']."', id_importancia = '".$_POST['imp']."' WHERE cd_compromisso = '".$cd."'";
+
+		if($result = $mysqli->query($edtar)){
+			echo "<script>window.location.href='index.php';</script>";
+		}
+		else{
+			printf("Error: %s\n", $mysqli->error);
+		}
+	}
+
+?>
 
 </body>
 </html>
